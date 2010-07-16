ca8d18f2798262b479dc7749b5cb3cb20da6b748
David Greaves <david@dgreaves.com>
2010-07-16 20:24:43 +0100

    Allow AIR python client to reuse an existing AMQP connection

--- python-air/AIR/AMQPServer.py
+++ python-air/AIR/AMQPServer.py
@@ -9,16 +9,20 @@ class AMQPServer(object):
     """
     def __init__(self, 
                  amqp_host = "localhost", amqp_user = "air",
-                 amqp_pass = "air", amqp_vhost = "AIR"):
-        self.host = amqp_host
-        self.user = amqp_user
-        self.pw = amqp_pass
-        self.vhost = amqp_vhost
-        self.conn = amqp.Connection(host=self.host,
-                                     userid=self.user,
-                                     password=self.pw,
-                                     virtual_host=self.vhost,
-                                     insist=False)
+                 amqp_pass = "air", amqp_vhost = "AIR",
+                 conn = None):
+        if conn != None:
+            self.conn = conn
+        else:            
+            self.host = amqp_host
+            self.user = amqp_user
+            self.pw = amqp_pass
+            self.vhost = amqp_vhost
+            self.conn = amqp.Connection(host=self.host,
+                                        userid=self.user,
+                                        password=self.pw,
+                                        virtual_host=self.vhost,
+                                        insist=False)
         if self.conn == None:
             raise Exception("No connection")
         self.chan = self.conn.channel()
